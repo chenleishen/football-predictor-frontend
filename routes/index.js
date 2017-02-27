3a9c51b3603bd998c22ff63ef8637973755af69d
Chenlei Shen <chenleishen@Chenleis-MacBook-Pro.local>
2017-02-26 23:16:29 -0500

    initialize mean stack

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	console.log('does it even get here?');
+	console.log(path.join(__dirname + '../index.html'));
+  res.sendFile(path.join(__dirname + '../index.html'));
+  // res.sendFile('index.html');
+});
+
+module.exports = router;
