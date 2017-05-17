dfa90095cc7e42cc4de42e54a387f7a224daea31
gauravlath07 <gauravlath1997@gmail.com>
2017-05-17 17:43:57 -0400

    add instructions

--- bin2/index.js
+++ bin2/index.js
@@ -110,6 +110,31 @@ exports.ChoosePlayersController = function($scope, $http) {
 
     $scope.scoreCalculated = false;
 
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
 	$scope.$on('show_players1', function (event,team_name) {
         $scope.show_choosePlayers1 = true;
 		$scope.team1_name = team_name;
